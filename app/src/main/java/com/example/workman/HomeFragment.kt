c30d12a67ee78f83465d4edefd031776917651a0
sbwoo96 <sbwoo1996@gmail.com>
2020-01-14 17:16:23 +0900

    update layout

--- app/src/main/java/com/example/workman/HomeFragment.kt
+++ app/src/main/java/com/example/workman/HomeFragment.kt
@@ -1,24 +1,95 @@
 package com.example.workman
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 /**
  * 홈
  */
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnMapReadyCallback{
+
+    private lateinit var mapView: MapView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val invite = rootView.findViewById<Button>(R.id.button)
+
+        // 초대하기 버튼 클릭
+        invite.setOnClickListener {
+            // 직원 초대하는 화면으로 이동
+            activity!!.supportFragmentManager.beginTransaction().replace(R.id.container, InviteFragment()).addToBackStack(null).commit()
+
+        }
+
+        return rootView
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mapView = view!!.findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+
+        val seoul = LatLng(37.566, 126.978)
+        p0.moveCamera(CameraUpdateFactory.newLatLng(seoul))
+        p0.moveCamera(CameraUpdateFactory.zoomTo(10f))
+
+        val marker =
+            MarkerOptions()
+                .position(seoul)
+                .title("서울")
+                .snippet("아름다운 도시")
+        p0.addMarker(marker)
+
+    }
+
+    override fun onStart() {
+        mapView.onStart()
+        super.onStart()
+    }
+
+    override fun onResume() {
+        mapView.onResume()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        mapView.onPause()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        mapView.onStop()
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        mapView.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        mapView.onLowMemory()
+        super.onLowMemory()
+    }
 
 }
